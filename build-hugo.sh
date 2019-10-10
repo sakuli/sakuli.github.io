4706fbd127c8c42ee998d3456a60ba46d748f7d9
Simon Hofmann <simon.hofmann@consol.de>
2019-10-10 14:10:56 +0200

    Explicitly calling /bin/hugo

--- build-hugo.sh
+++ build-hugo.sh
@@ -7,10 +7,9 @@ tar -C /usr/local -xzf go.tar.gz
 
 # Set up paths
 export GOPATH=$HOME/go
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
 # Build Hugo
 git clone --branch v0.56.3 --depth 1 https://github.com/gohugoio/hugo.git
 cd hugo
 GO111MODULE=on go install --tags extended
-hugo version
+$GOPATH/bin/hugo version
--- build-site.sh
+++ build-site.sh
@@ -2,9 +2,8 @@
 
 set -e
 export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
 cd vue-components
 npm ci
 npm run build
 cd -
-hugo
+$GOPATH/bin/hugo
